295fc5a1260b46b82a85e101dc7ffd1ea12e68c6
kunalgupta022 <77953444+kunalgupta022@users.noreply.github.com>
2021-04-29 12:41:16 +0530

    Create a GH Action to check for trailing lines and EOFs (#2)
    * Remove dist form gitignore
    
    * Add Action FIles
    
    * Generate distribution
    
    * Change description in action.yml
    
    * Update README.md
    
    * Remove redundant logic
    
    * Generate distribution
    
    * Git add remote
    
    * Generate distribution
    
    * Add url var
    
    * Generate distribution
    
    * Add try except to set action as failed when error
    
    * Added fetch and checkout to simplegit setup
    
    * Generate distribution
    
    * Add commit message as action input
    
    * Generate distribution
    
    * Use Json array  as input for ignore file paths
    
    * Generate distribution
    
    * Parse ignore file patterns
    
    * Generate distribution
    
    * Fix log messages
    
    * Generate distribution
    
    * Update readme
    
    * Generate distribution
    
    * Update action.yml description
    
    Co-authored-by: Gopal Sharma <gps@surya-soft.com>
    
    * Update decription and commit messages in readme
    
    * Change default message and push inputs to top
    
    * Generate distribution
    
    * Remove author in action.yml
    
    * Add error handling for push to master event
    
    * Generate distribution
    
    * Update readme
    
    * Use core.error for exiting on incompatible run
    
    * Generate distribution
    
    * Refactor code
    
    * Generate distribution
    
    * Fix required funtions parameters
    
    * Generate distribution
    
    Co-authored-by: kunalgupta022 <kunalgupta022@users.noreply.github.com>
    Co-authored-by: Gopal Sharma <gps@surya-soft.com>

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # newline-at-eof
-A GitHub Action that ensures that files end with exactly 1 newline at end of file
+
+A GitHub Action that ensures that files end with exactly 1 newline at end of file.
+
+This Action runs only on files modified in `Pull Requests`. It will fail if executed on other events.
+
+## Inputs
+
+### `GH_TOKEN`
+
+The GitHub token used to authenticate with GitHub.
+
+**Required**
+
+### `IGNORE_FILE_PATTERNS`
+
+Path to ignore while checkking for newline at EOF.
+
+**Optional**
+
+### `COMMIT_MESSAGE`
+
+Commit message to post when making a fix commit
+
+**Required**
+
+**Default Value**
+
+If unspecified, it defaults to the following message:
+
+"Fix formatting"
+
+## Example Usage
+
+```yml
+- name: Check newline at EOF
+  uses: gps/newline-at-eof@master
+  with:
+    GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    IGNORE_FILE_PATTERNS: |
+      [
+        "dist/.*",
+        "package-lock.json"
+      ]
+    COMMIT_MESSAGE: 'Fix formatting'
+```
---
+++ index.js
@@ -0,0 +1,167 @@
+const exec = require('@actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { parse: gitDiffParser } = require('what-the-diff');
+const fs = require('fs-extra');
+const simpleGit = require('simple-git');
+const env = process.env;
+
+function matchExact(r, str) {
+  let match = str.match(r);
+  return match && str === match[0];
+}
+
+function fixNewLineEOF(b) {
+  // Replace all trailing whitespaces
+  b = b.replace(/[ \t\n]*$/, '\n');
+
+  if (b.length === 1) {
+    // if the remaining character is a whitespace
+    if (/[ \t\n]*$/.test(b)) {
+      return '';
+    } else {
+      return b + '\n';
+    }
+  }
+  return b;
+}
+
+async function getChangedFilesPaths(token) {
+  const octokit = github.getOctokit(token);
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  const owner = env.GITHUB_REPOSITORY.split('/')[0];
+  const repo = env.GITHUB_REPOSITORY.split('/')[1];
+
+  const { data: pullRequestDiff } = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: pull_request.number,
+    mediaType: {
+      format: 'diff'
+    }
+  });
+
+  const parsedDiff = gitDiffParser(pullRequestDiff);
+
+  const changedFilePaths = parsedDiff.map((e) => {
+    return e['newPath'].replace('b/', '');
+  });
+
+  return changedFilePaths;
+}
+
+async function checkoutToBranch(branch, token) {
+  const url = `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}.git`.replace(
+    /^https:\/\//,
+    `https://x-access-token:${token}@`
+  );
+
+  const git = simpleGit();
+  await git.addRemote('repo', url);
+  await git.fetch('repo');
+  await git.checkout(branch);
+  return git;
+}
+
+function checkFilesForEOF(filesToCheck) {
+  const filesToCommit = [];
+
+  for (let i = 0; i < filesToCheck.length; i++) {
+    if (filesToCheck[i] !== null) {
+      const data = fs.readFileSync(filesToCheck[i], {
+        encoding: 'utf8',
+        flag: 'r'
+      });
+      const fixedData = fixNewLineEOF(data);
+      if (data !== fixedData) {
+        filesToCommit.push(filesToCheck[i]);
+        fs.writeFileSync(filesToCheck[i], fixedData, 'utf8');
+      }
+    }
+  }
+
+  return filesToCommit;
+}
+
+async function commitChanges(filesToCommit, commitMessage, git, branch) {
+  const diff = await exec.exec('git', ['diff', '--quiet'], {
+    ignoreReturnCode: true
+  });
+
+  if (diff) {
+    await core.group('push changes', async () => {
+      await git.addConfig(
+        'user.email',
+        `${env.GITHUB_ACTOR}@users.noreply.github.com`
+      );
+      await git.addConfig('user.name', env.GITHUB_ACTOR);
+      await git.add(filesToCommit);
+      await git.commit(commitMessage);
+      await git.push('repo', branch);
+    });
+  } else {
+    console.log('No changes to make');
+  }
+}
+
+async function run() {
+  const token = core.getInput('GH_TOKEN');
+  let ignorePaths = core.getInput('IGNORE_FILE_PATTERNS');
+  let commitMessage = core.getInput('COMMIT_MESSAGE');
+
+  if (!ignorePaths) {
+    ignorePaths = [];
+  } else {
+    ignorePaths = JSON.parse(ignorePaths);
+  }
+  core.info('Ignore File Patterns: ' + JSON.stringify(ignorePaths));
+
+  if (!commitMessage) {
+    commitMessage = 'Fix formatting';
+  }
+
+  try {
+    // Extract branch name if pull request else break execution.
+    let branch;
+    if (github.context.eventName == 'pull_request') {
+      branch = github.context.payload.pull_request.head.ref;
+    } else {
+      core.error('This action will only work on Pull Requests. Exiting.');
+      return;
+    }
+
+    const git = await checkoutToBranch(branch, token);
+
+    // Extract files that changed in PR.
+    const changedFilePaths = await getChangedFilesPaths(token);
+
+    core.info('Changed files paths: ' + JSON.stringify(changedFilePaths));
+
+    // Remove files matching ignore paths regex
+    const filesToCheck = changedFilePaths.map((e) => {
+      for (let i = 0; i < ignorePaths.length; i++) {
+        if (matchExact(ignorePaths[i], e)) {
+          return null;
+        }
+      }
+      return e;
+    });
+
+    core.info('Files to check: ' + JSON.stringify(filesToCheck));
+
+    // Store modified files
+    const filesToCommit = checkFilesForEOF(filesToCheck);
+
+    // Log Changed files
+    core.info('Files to commit: ' + JSON.stringify(filesToCommit));
+
+    // Generate DIff and commit changes
+    await commitChanges(filesToCommit, commitMessage, git, branch);
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
+
+run();
